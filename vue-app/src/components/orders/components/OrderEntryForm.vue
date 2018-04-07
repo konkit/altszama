f0cafd6c0535756c4cc5f0f62daa908fe0bc10dd
Łukasz Tenerowicz <konkit@gmail.com>
2018-04-07 16:55:41 +0200

    Improve showEntry view

--- vue-app/src/components/orders/components/OrderEntryForm.vue
+++ vue-app/src/components/orders/components/OrderEntryForm.vue
@@ -1,90 +1,95 @@
 <template>
-<div class="container">
-  <div class="row justify-content-center">
-    <div class="col-8">
+  <div class="container">
 
-      <div class="form-group">
-        <h4>Dish</h4>
+    <div class="row justify-content-center" v-if="!this.newDish">
+      <div class="col-8">
+        <div class="form-group">
+          <h4>Dish</h4>
 
-        <select class="form-control" required="" id="dish" v-model="orderEntry.dishId" @change="clearSideDishes">
-          <optgroup v-for='(dishEntry, i) in this.allDishesByCategory' :key='i' :label="dishEntry.category">
-            <option v-for='(dish, i) in dishEntry.dishes' :key='i' :value="dish.id">
-              {{dish.name}}&nbsp;( <price :data-price="dish.price" /> )
-            </option>
-          </optgroup>
-        </select>
+          <select class="form-control" required="" id="dish" v-model="orderEntry.dishId" @change="clearSideDishes">
+            <optgroup v-for='(dishEntry, i) in this.allDishesByCategory' :key='i' :label="dishEntry.category">
+              <option v-for='(dish, i) in dishEntry.dishes' :key='i' :value="dish.id">
+                {{dish.name}}&nbsp;( <price :data-price="dish.price" /> )
+              </option>
+            </optgroup>
+          </select>
+        </div>
+      </div>
+
+      <div class="col-4">
+        <h3>&nbsp;</h3>
+        <p><button class="btn btn-link" v-on:click="setNewDishFlag(true)">Add new dish! &nbsp;</button></p>
       </div>
     </div>
 
-    <div class="col-4">
-      <h3>&nbsp;</h3>
-      <p>Can't find your dish ? <button class="btn btn-link" v-on:click="this.goToCreateDish">Add one now! &nbsp;</button></p>
+    <div class="row justify-content-center" v-if="this.newDish">
+      <div class="col-8">
+        <div class="form-group">
+          <h4>Dish</h4>
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="New dish name" id="newDishName" v-model="orderEntry.newDishName" />
+            <vue-numeric currency="zł" separator="." currency-symbol-position="suffix" v-model="price" v-bind:precision="2" class="form-control" required="" id="price"></vue-numeric>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-4">
+        <h3>&nbsp;</h3>
+        <p><button class="btn btn-link" v-on:click="setNewDishFlag(false)">Back to dish list &nbsp;</button></p>
+      </div>
     </div>
-  </div>
 
-  <div class="row justify-content-center">
-    <div class="col-8">
-      <div class="form-group">
-        <div>
-          <h4>Side dishes</h4>
 
-          <div v-if="this.orderEntry.chosenSideDishes.length > 0"> 
-            <p v-for="sideDish in this.orderEntry.chosenSideDishes" v-bind:key="sideDish.id">
-              {{sideDish.name}}&nbsp;( <price v-bind:data-price="sideDish.price" /> )
+    <div class="row justify-content-center">
+      <div class="col-8">
+        <div class="form-group">
+          <div>
+            <h4>Side dishes</h4>
 
-              <span class="fa fa-remove" v-on:click="removeSideDish(sideDish.id)"></span>
-            </p>
-          </div>
-          <div v-else>
-            <p>No side dishes selected</p>
-          </div>
+            <div v-if="orderEntry.chosenSideDishes.length > 0">
+              <div v-for="(sideDish, i) in orderEntry.chosenSideDishes" :key="i">
+                
+                <div class="input-group">
+                  <select class="form-control" name="sideDishId" required="" v-model="orderEntry.chosenSideDishes[i]">
+                    <option v-for="sideDish in dishIdToSideDishesMap[orderEntry.dishId]" :key="sideDish.id" :value="sideDish">
+                      {{sideDish.name}}&nbsp;( <price :data-price="sideDish.price" /> )
+                    </option>
+                  </select>
+                  
+                  <button @click="removeSideDish(sideDish.id)"><span class="fa fa-remove" /></button>
+                </div>
 
-          <div v-if="this.sideDishFormVisible === false">
-            <button class="btn btn-success" v-on:click="setSideDishFormVisible(true)">
+              </div>
+            </div>
+            <div v-else>
+              <p>No side dishes selected</p>
+            </div>
+
+            <button class="btn btn-success" @click="addSideDishEntry()">
               Add side dish &nbsp; <i class="fa fa-plus" />
             </button>
           </div>
+        </div>
+      </div>
 
-          <div v-if="this.sideDishFormVisible === true">
-            <select class="form-control" name="sideDishId" required="" v-model="sideDishIdToAdd">
-              <option v-for="sideDish in this.dishIdToSideDishesMap[this.orderEntry.dishId]" v-bind:key="sideDish.id" v-bind:value="sideDish.id">
-                {{sideDish.name}}&nbsp;( <price v-bind:data-price="sideDish.price" /> )
-              </option>
-            </select>
+      <div class="col-4">
+        <!-- <h3>&nbsp;</h3>
+        <p>
+          Can't find your favourite sidedish ? 
+          <button class="btn btn-link" @click="this.goToCreateDish">Add one now! &nbsp;</button>
+        </p> -->
+      </div>
+    </div>
 
-            <div class="input-group">
-              <button class="btn btn-success" v-on:click="addSideDish">
-                Add &nbsp; <i class="fa fa-plus" />
-              </button>
-              
-              <button class="btn btn-secondary" v-on:click="setSideDishFormVisible(false)">
-                Cancel
-              </button>
-            </div>
-          </div>
-
+    <div class="row justify-content-center">
+      <div class="col">
+        <div class="form-group">
+          <h4>Additional Comments</h4>
+          <textarea class="form-control" name="additionalComments" value="" id="additionalComments" v-model="orderEntry.additionalComments" />
         </div>
       </div>
     </div>
-
-    <div class="col-4">
-      <h3>&nbsp;</h3>
-      <p>
-        Can't find your favourite sidedish ? 
-        <button class="btn btn-link" v-on:click="this.goToCreateDish">Add one now! &nbsp;</button>
-      </p>
-    </div>
   </div>
-
-  <div class="row justify-content-center">
-    <div class="col">
-      <div class="form-group">
-        <h4>Additional Comments</h4>
-        <textarea class="form-control" name="additionalComments" value="" id="additionalComments" v-model="orderEntry.additionalComments" />
-      </div>
-    </div>
-  </div>
-</div>
 </template>
 
 
@@ -116,7 +121,9 @@ export default {
   data () {
     return {
       sideDishIdToAdd: '',
-      sideDishFormVisible: false
+      sideDishFormVisible: false,
+
+      newDish: false
     }
   },
   methods: {
@@ -144,6 +151,16 @@ export default {
     },
     isSelected: function(currentDishId) {
       return currentDishId === this.orderEntry.dish.id;
+    },
+    setNewDishFlag: function(newDishValue) {
+      this.newDish = newDishValue;
+    },
+    addSideDishEntry: function() {
+      var currentDishId = this.orderEntry.dishId
+      var sideDishToAdd = this.dishIdToSideDishesMap[currentDishId][0]
+      var chosenSideDishes = this.orderEntry.chosenSideDishes
+
+      chosenSideDishes.push(sideDishToAdd);  
     }
   },
   components: {
