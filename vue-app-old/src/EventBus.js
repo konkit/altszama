634fc50f8a61e62cd4ab30fce5d68439a04c985f
Łukasz Tenerowicz <konkit@gmail.com>
2018-12-05 20:45:25 +0100

    Add successfully migrated app to vue-cli 3

--- vue-app-old/src/EventBus.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-
-var eventBus = new Vue()
-
-export default {
-    addEvent: (eventName, eventAction) => eventBus.$on(eventName, eventAction),
-    emitEvent: (eventName) => eventBus.$emit(eventName)
-}
