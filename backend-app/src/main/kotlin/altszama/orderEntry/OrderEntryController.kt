7d9f8a82fe3b90e5556d3397a7f04b7725436df8
Łukasz Tenerowicz <konkit@gmail.com>
2019-01-06 13:11:21 +0100

    Fixes

--- backend-app/src/main/kotlin/altszama/orderEntry/OrderEntryController.kt
+++ backend-app/src/main/kotlin/altszama/orderEntry/OrderEntryController.kt
@@ -18,31 +18,31 @@ class OrderEntryController {
   private lateinit var orderEntryService: OrderEntryService
 
 
-  @RequestMapping(value = "/order_entries/save")
+  @RequestMapping("/order_entries/save")
   fun save(@RequestBody @Valid saveRequest: OrderEntrySaveRequest): ResponseEntity<String> {
     orderEntryService.saveEntry(saveRequest)
     return ResponseEntity(HttpStatus.OK)
   }
 
-  @RequestMapping(value = "/order_entries/update")
+  @RequestMapping("/order_entries/update")
   fun update(@RequestBody @Valid updateRequest: OrderEntryUpdateRequest): ResponseEntity<String> {
     orderEntryService.updateEntry(updateRequest)
     return ResponseEntity(HttpStatus.OK)
   }
 
-  @RequestMapping(value = "/order_entries/{orderEntryId}/dish_entry/{dishEntryId}/delete")
+  @RequestMapping("/order_entries/{orderEntryId}/dish_entry/{dishEntryId}/delete")
   fun delete(@PathVariable orderEntryId: String, @PathVariable dishEntryId: String): ResponseEntity<String> {
     orderEntryService.deleteOrderEntry(orderEntryId, dishEntryId)
     return ResponseEntity(HttpStatus.OK)
   }
 
-  @RequestMapping(value = "/order_entries/{orderEntryId}/mark_as_paid")
+  @RequestMapping("/order_entries/{orderEntryId}/mark_as_paid")
   fun setAsMarkedAsPaid(@PathVariable orderEntryId: String): ResponseEntity<String> {
     orderEntryService.setAsMarkedAsPaid(orderEntryId)
     return ResponseEntity(HttpStatus.OK)
   }
 
-  @RequestMapping(value = "/order_entries/{orderEntryId}/confirm_as_paid")
+  @RequestMapping("/order_entries/{orderEntryId}/confirm_as_paid")
   fun setAsConfirmedAsPaid(@PathVariable orderEntryId: String): ResponseEntity<String> {
     orderEntryService.setAsConfirmedAsPaid(orderEntryId)
     return ResponseEntity(HttpStatus.OK)
