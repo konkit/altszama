f1976cd92f38ef0b30326dc0162de76ff9a6b44f
Łukasz Tenerowicz <konkit@gmail.com>
2020-06-24 20:37:38 +0200

    Add typescript support

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,49 +1,32 @@
 <template>
-    <v-app>
-      <Navigation></Navigation>
-
-      <router-view></router-view>
-    </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-  import Router from 'vue-router'
-  import Navigation from './views/commons/Navigation.vue'
-  import ApiConnector from "./lib/ApiConnector";
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  export default {
-    name: 'app',
-    data: () => ({
-      drawer: null
-    }),
-    computed: {
-      username() {
-        return this.$store.state.username;
-      },
-      masterNavDrawerOpened() {
-        return this.$store.state.masterNavDrawerOpened
-      }
-    },
-    methods: {
-      shouldDisplayNavigation() {
-        return this.$route.name !== "Login"
-      },
-      logout() {
-        ApiConnector.logout();
-      },
-      goToPath(path) {
-        this.$router.push(path)
-      },
-      setMasterNavDrawerOpened(newValue) {
-        this.$store.commit("setMasterNavigationDrawerOpened", newValue);
-      }
-    },
-    components: {
-      Navigation,
-      Router
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
-</script>
-
-<style scoped>
+}
 </style>
