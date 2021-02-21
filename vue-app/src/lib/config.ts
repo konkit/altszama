db2238b690d0f65bb17d6f5437633bc56de06c88
Łukasz Tenerowicz <konkit@gmail.com>
2021-02-21 23:19:07 +0100

    Fix config

--- vue-app/src/lib/config.ts
+++ vue-app/src/lib/config.ts
@@ -1,10 +1,10 @@
 declare global {
-  interface Window { _env: any; }
+  interface Window { _env?: any; }
 }
 
 export const CURRENT_DOMAIN = location.protocol + "//" + location.hostname + (location.port ? ":" + location.port : "");
 
-export const BACKEND_URL = window._env.VUE_APP_BACKEND_URL || process.env.VUE_APP_BACKEND_URL || CURRENT_DOMAIN;
-export const VAPID_PUBLIC_KEY = window._env.VUE_APP_VAPID_PUBLIC_KEY || process.env.VUE_APP_VAPID_PUBLIC_KEY;
-export const GOOGLE_CLIENT_ID = window._env.VUE_APP_GOOGLE_CLIENT_ID || process.env.VUE_APP_GOOGLE_CLIENT_ID;
-export const SENTRY_URL = window._env.VUE_APP_SENTRY_URL || process.env.VUE_APP_SENTRY_URL;
+export const BACKEND_URL = window._env?.VUE_APP_BACKEND_URL || process.env.VUE_APP_BACKEND_URL || CURRENT_DOMAIN;
+export const VAPID_PUBLIC_KEY = window._env?.VUE_APP_VAPID_PUBLIC_KEY || process.env.VUE_APP_VAPID_PUBLIC_KEY;
+export const GOOGLE_CLIENT_ID = window._env?.VUE_APP_GOOGLE_CLIENT_ID || process.env.VUE_APP_GOOGLE_CLIENT_ID;
+export const SENTRY_URL = window._env?.VUE_APP_SENTRY_URL || process.env.VUE_APP_SENTRY_URL;
