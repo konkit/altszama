5158f920dd3333b47afc8efabda086f718e99181
Łukasz Tenerowicz <konkit@gmail.com>
2021-04-11 17:14:26 +0200

    Add indices for dish

--- backend-app/src/main/kotlin/altszama/app/dish/Dish.kt
+++ backend-app/src/main/kotlin/altszama/app/dish/Dish.kt
@@ -3,10 +3,17 @@ package altszama.app.dish
 import altszama.app.restaurant.Restaurant
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.CompoundIndex
+import org.springframework.data.mongodb.core.index.CompoundIndexes
 import org.springframework.data.mongodb.core.mapping.DBRef
+import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
 import javax.validation.constraints.NotNull
 
+@Document
+@CompoundIndexes(value = [
+  CompoundIndex(name = "restaurant_id_to_restaurant", def = "{'restaurant.\$id' : 1}")
+])
 data class Dish(
   @DBRef
   @NotNull
