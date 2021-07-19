8f5f4e3d01329df3df7cddf57bfcb1b7b6ef04e8
Łukasz Tenerowicz <konkit@gmail.com>
2021-07-20 00:21:13 +0200

    Fix e2e tests

--- e2e/rune2e.sh
+++ e2e/rune2e.sh
@@ -2,6 +2,9 @@ cd ../
 ./buildDockerImages.sh
 TARGET_PORT=8099 docker-compose -p TEST up -d
 cd ./e2e
+
+sleep 5
+
 TARGET_PORT=8099 npx testcafe chrome src/tests/ --debug-on-fail
 cd ../
 TARGET_PORT=8099 docker-compose -p TEST down
