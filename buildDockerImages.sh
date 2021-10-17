979f03dfd0d8369cdf69e83f7c746f23fe0a135c
Łukasz Tenerowicz <konkit@gmail.com>
2021-10-17 23:26:02 +0200

    Build images to dockerhub instead of just locally

--- buildDockerImages.sh
+++ buildDockerImages.sh
@@ -1,8 +1,21 @@
-cd vue-app
-docker build -t altszama-frontend:latest .
-cd ../reverse-proxy
-docker build -t altszama-reverse-proxy:latest .
-cd ../backend-app
-./gradlew bootBuildImage
+if [[ -n "$CIRCLE_BRANCH" ]]; then
+  TAG="latest-${CIRCLE_BRANCH}"
+else
+  TAG="latest-master"
+fi
+
+cd ./vue-app || return 1
+docker build -t konkit/altszama-frontend:${TAG} .
+docker push konkit/altszama-frontend:${TAG} .
+cd ../
+
+cd ./reverse-proxy || return 1
+docker build -t konkit/altszama-reverse-proxy:${TAG} .
+docker push konkit/altszama-reverse-proxy:${TAG} .
+cd ../
+
+cd ./backend-app || return 1
+./gradlew bootBuildImage --imageName=konkit/altszama-backend:${TAG} .
+docker push konkit/altszama-backend:${TAG} .
 cd ../
 
