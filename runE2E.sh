cb0171b4703cab472d1a5d751dc3abf3f90301be
Łukasz Tenerowicz <konkit@gmail.com>
2021-10-18 20:09:19 +0200

    Add e2e tests to CircleCI run

--- runE2E.sh
+++ runE2E.sh
@@ -9,6 +9,22 @@ set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
+# Fill .env file if it doesn't exist
+if [ ! -f ".env" ]; then
+  echo "DATABASE_NAME=$DATABASE_NAME" >> .env
+  echo "DATABASE_HOST=$DATABASE_HOST" >> .env
+  echo "DATABASE_PORT=$DATABASE_PORT" >> .env
+  echo "GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID" >> .env
+  echo "GOOGLE_CLIENT_SECRET=$GOOGLE_CLIENT_SECRET" >> .env
+  echo "VAPID_PUBLIC_KEY=$VAPID_PUBLIC_KEY" >> .env
+  echo "VAPID_PRIVATE_KEY=$VAPID_PRIVATE_KEY" >> .env
+  echo "VAPID_SUBJECT=$VAPID_SUBJECT" >> .env
+  echo "ORIGIN_URL=$ORIGIN_URL" >> .env
+  echo "JWT_SIGNING_KEY=$JWT_SIGNING_KEY" >> .env
+  echo "SENTRY_URL=$SENTRY_URL" >> .env
+  echo "SERVER_PORT=$SERVER_PORT" >> .env
+fi
+
 
 export TARGET_PORT=8099
 export DOCKER_TAG="latest-${CIRCLE_BRANCH}"
