d958b778e1cb3904e07206b90413f1aac376e504
Łukasz Tenerowicz <konkit@gmail.com>
2021-10-18 18:10:57 +0200

    Fix CircleCI script

--- buildDockerImages.sh
+++ buildDockerImages.sh
@@ -1,8 +1,17 @@
-if [[ -n "$CIRCLE_BRANCH" ]]; then
-  TAG="latest-${CIRCLE_BRANCH}"
-else
-  TAG="latest-master"
-fi
+#!/usr/bin/env bash
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+
+
+TAG="latest-${CIRCLE_BRANCH}"
 
 cd ./vue-app || return 1
 docker build -t konkit/altszama-frontend:${TAG} .
