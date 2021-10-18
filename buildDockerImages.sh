31694f56a912d7e95a9144dc89f5b585aba7669d
Łukasz Tenerowicz <konkit@gmail.com>
2021-10-18 18:03:55 +0200

    Fix CircleCI script

--- buildDockerImages.sh
+++ buildDockerImages.sh
@@ -6,16 +6,16 @@ fi
 
 cd ./vue-app || return 1
 docker build -t konkit/altszama-frontend:${TAG} .
-docker push konkit/altszama-frontend:${TAG} .
+docker push konkit/altszama-frontend:${TAG}
 cd ../
 
 cd ./reverse-proxy || return 1
 docker build -t konkit/altszama-reverse-proxy:${TAG} .
-docker push konkit/altszama-reverse-proxy:${TAG} .
+docker push konkit/altszama-reverse-proxy:${TAG}
 cd ../
 
 cd ./backend-app || return 1
 ./gradlew bootBuildImage --imageName=konkit/altszama-backend:${TAG} .
-docker push konkit/altszama-backend:${TAG} .
+docker push konkit/altszama-backend:${TAG}
 cd ../
 
