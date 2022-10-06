bd31f23620ff4cd6aaf6a43f1e38b1641b04da76
Łukasz Tenerowicz <konkit@gmail.com>
2022-10-06 23:14:15 +0200

    Upgrade vue-cli

--- generateSwaggerClient.sh
+++ generateSwaggerClient.sh
@@ -1,4 +1,4 @@
-java -jar swagger-codegen-cli.jar generate \
+java -jar swagger-codegen-cli-3.0.35.jar generate \
    -i http://localhost:8088/api/swagger/frontend \
    -l typescript-fetch \
    -o ./vue-app/src/frontend-client/
