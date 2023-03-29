1ed5ff684d35edf82f2fde70c5d0e2d3ae6b33ac
Łukasz Tenerowicz <konkit@gmail.com>
2023-03-29 22:35:27 +0200

    Fix balance view

--- frontend-angular/src/app/views/balance/balance-view/balance-view.component.ts
+++ frontend-angular/src/app/views/balance/balance-view/balance-view.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import {OrderHistoryCreatedEntry, OrderHistoryParticipatedEntry} from "../../../../frontend-client";
+import {Component, OnInit} from '@angular/core';
+import {
+  BalanceControllerService,
+  OrderHistoryCreatedEntry,
+  OrderHistoryParticipatedEntry
+} from "../../../../frontend-client";
 
 @Component({
   selector: 'app-balance-view',
   templateUrl: './balance-view.component.html',
   styleUrls: ['./balance-view.component.scss']
 })
-export class BalanceViewComponent {
+export class BalanceViewComponent implements OnInit {
+  isLoading = true
   orderHistoryEntries: (OrderHistoryCreatedEntry | OrderHistoryParticipatedEntry)[] = []
   owedMoneyEntries: [string, number][] = []
+
+  constructor(private balanceControllerService: BalanceControllerService) {
+  }
+
+  ngOnInit() {
+    this.balanceControllerService.getBalanceForUser()
+      .subscribe(response => {
+        this.orderHistoryEntries = response.entries
+        this.owedMoneyEntries = Object.entries(response.owedMoney)
+        this.isLoading = false
+      })
+  }
 
   isCreatedEntry(entry: OrderHistoryCreatedEntry | OrderHistoryParticipatedEntry): entry is OrderHistoryCreatedEntry {
     return entry.kind == "createdEntry"
