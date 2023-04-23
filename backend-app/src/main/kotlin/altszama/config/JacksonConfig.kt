1c6080d31191af9ec5c76544add6cbc44e8412a6
Łukasz Tenerowicz <konkit@gmail.com>
2023-04-23 13:12:03 +0200

    Refactor backend app dependencies a bit

--- backend-app/src/main/kotlin/altszama/config/JacksonConfig.kt
+++ backend-app/src/main/kotlin/altszama/config/JacksonConfig.kt
@@ -4,18 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
-import org.springdoc.core.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
-import org.springframework.web.servlet.mvc.method.RequestMappingInfo.paths
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.LocalTime
-import java.time.ZonedDateTime
 
 
 @Configuration
@@ -26,7 +20,7 @@ open class JacksonConfig {
   open fun objectMapper(builder: Jackson2ObjectMapperBuilder): ObjectMapper {
     val objectMapper = builder.createXmlMapper(false).build<ObjectMapper>()
 
-    objectMapper.registerModule(KotlinModule())
+    objectMapper.registerKotlinModule()
     objectMapper.registerModule(ParameterNamesModule())
     objectMapper.registerModule(Jdk8Module())
     objectMapper.registerModule(JavaTimeModule())
