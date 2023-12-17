dd8396f7eceb402be1ff6b93a25a6820a556e3ca
Łukasz Tenerowicz <konkit@gmail.com>
2023-12-17 23:11:10 +0100

    Remove order history from the balance tab to improve performance

--- frontend-angular/src/app/views/balance/balance-view/balance-view.component.ts
+++ frontend-angular/src/app/views/balance/balance-view/balance-view.component.ts
@@ -1,9 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {
-  BalanceControllerService,
-  OrderHistoryCreatedEntry,
-  OrderHistoryParticipatedEntry
-} from "../../../../frontend-client";
+import {BalanceControllerService,} from "../../../../frontend-client";
 
 @Component({
   selector: 'app-balance-view',
@@ -12,7 +8,6 @@ import {
 })
 export class BalanceViewComponent implements OnInit {
   isLoading = true
-  orderHistoryEntries: (OrderHistoryCreatedEntry | OrderHistoryParticipatedEntry)[] = []
   owedMoneyEntries: [string, number][] = []
 
   constructor(private balanceControllerService: BalanceControllerService) {
@@ -21,17 +16,8 @@ export class BalanceViewComponent implements OnInit {
   ngOnInit() {
     this.balanceControllerService.getBalanceForUser()
       .subscribe(response => {
-        this.orderHistoryEntries = response.entries
         this.owedMoneyEntries = Object.entries(response.owedMoney)
         this.isLoading = false
       })
-  }
-
-  isCreatedEntry(entry: OrderHistoryCreatedEntry | OrderHistoryParticipatedEntry): entry is OrderHistoryCreatedEntry {
-    return entry.kind == "createdEntry"
-  }
-
-  isParticipatedEntry(entry: OrderHistoryCreatedEntry | OrderHistoryParticipatedEntry): entry is OrderHistoryParticipatedEntry {
-    return entry.kind == "participatedEntry"
   }
 }
