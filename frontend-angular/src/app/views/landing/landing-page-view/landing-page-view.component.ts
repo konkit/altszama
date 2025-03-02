5a190096c3271756a8bed51edf445072bbc6a349
Łukasz Tenerowicz <konkit@gmail.com>
2025-03-02 16:55:52 +0100

    Revamp Landing Page

--- frontend-angular/src/app/views/landing/landing-page-view/landing-page-view.component.ts
+++ frontend-angular/src/app/views/landing/landing-page-view/landing-page-view.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-landing-page-view',
-  imports: [RouterModule],
+  imports: [RouterModule, MatButtonModule],
   templateUrl: './landing-page-view.component.html',
-  styleUrl: './landing-page-view.component.scss'
+  styleUrl: './landing-page-view.component.scss',
+  standalone: true,
 })
 export class LandingPageViewComponent {
 
